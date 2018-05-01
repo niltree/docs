a747a2c762a8154b43b2afaa650d8cd1048eb8bc
Shawn Presser <shawnpresser@gmail.com>
2018-04-30 21:27:16 -0500

    Niltree WIP: API update

--- pages/_document.js
+++ pages/_document.js
@@ -32,8 +32,8 @@ export default class Document extends Document_ {
             _paq.push(['trackPageView']);
             _paq.push(['enableLinkTracking']);
             (function() {
-              var u="//zeit.co/api/_/";
-              _paq.push(['setTrackerUrl', u+'p']);
+              var u="//next.niltree.com/p/";
+              _paq.push(['setTrackerUrl', u+'p.php']);
               _paq.push(['setSiteId', '1']);
               var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
               g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'p.js'; s.parentNode.insertBefore(g,s);
