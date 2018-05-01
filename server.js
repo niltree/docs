cd70249edb28d6f4657d651875e20c5c02a0a275
Shawn Presser <shawnpresser@gmail.com>
2018-05-01 00:19:55 -0500

    Niltree website WIP

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.prepare().then(() => {
       } else {
         // Set the assetPrefix for now
         // It needs to be the https version, since now is always HTTPS
-        app.setAssetPrefix(`http://${req.headers.host}`)
+        app.setAssetPrefix(`https://${req.headers.host}`)
       }
 
       handleNextRequests(req, res, parsedUrl)
